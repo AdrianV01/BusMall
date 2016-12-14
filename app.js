bcc2494d02d95dbcffdf992d1ea6d88acaf0325b
Adrian <JS02137@gmail.com>
2016-12-14 08:58:03 -0800

    Added some pseudocode to outline things

---
+++ app.js
@@ -0,0 +1,5 @@
+//Constructor function to include:
+//Means of preventing the duplicates within selections
+//Means of preventing consecutive duplicates by position
+//this will involve the usage of two different arrays
+//
